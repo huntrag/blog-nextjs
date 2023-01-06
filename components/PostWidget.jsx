34f227fd5c3ddbbb4d1c493b8043e8b8f4d21277
Raghav Gade <r29aghavgade@gmail.com>
2023-01-06 16:48:14 +0530

    added postwidgets

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,8 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import moment from "moment";
 
-const PostWidget = () => {
-  return <div>PostWidget</div>;
+import { getRecentPosts, getSimilarPosts } from "../services";
+
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setrelatedPosts] = useState([]);
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) => {
+        setrelatedPosts(result);
+      });
+    } else {
+      getRecentPosts().then((result) => {
+        setrelatedPosts(result);
+      });
+    }
+  }, [slug]);
+
+  console.log(relatedPosts);
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        {slug ? "Related Posts" : "Recent Posts"}
+      </h3>
+      {relatedPosts.map((post) => (
+        <div key={post.title} className="flex items-center w-full mb-4">
+          <div className="w-16 flex-none">
+            <img
+              alt={post.title}
+              height="60px"
+              weight="60px"
+              className="align-middle rounded-full"
+              src={post.featuredImage.url}
+            />
+          </div>
+          <div className="flex-grow ml-4">
+            <p className="text-gray-500 font-xs">
+              {moment(post.createdAt).format("MMM-DD, YYYY")}
+            </p>
+            <Link
+              href={`/post/${post.slug}`}
+              key={post.title}
+              className="text-md"
+            >
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PostWidget;
