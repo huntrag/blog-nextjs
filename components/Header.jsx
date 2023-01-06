1605b5daa70593bd1887ce62149cb421b0563f49
Raghav Gade <r29aghavgade@gmail.com>
2023-01-06 17:15:22 +0530

    added categories

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,16 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Link from "next/link";
 
-const categories = [
-  { name: "React", slug: "react" },
-  { name: "Web Developement", slug: "web-dev" },
-];
+import { getCategories } from "../services";
 
 const Header = () => {
+  const [categories, setcategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setcategories(newCategories);
+    });
+  }, []);
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
